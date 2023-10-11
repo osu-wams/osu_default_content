052fb9c63db392ed445c07108b2c9ae6821923f2
Matthew Brabham <matthew.brabham@oregonstate.edu>
2023-10-11 16:54:06 -0700

    Generated Service.

--- src/LibraryMediaUpdate.php
+++ src/LibraryMediaUpdate.php
@@ -2,7 +2,35 @@
 
 namespace Drupal\osu_default_content;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+/**
+ * Service description.
+ */
 class LibraryMediaUpdate {
 
-}
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
 
+  /**
+   * Constructs a LibraryMediaUpdate object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    // @DCG place your code here.
+  }
+
+}
