7520db9b62e3ace07954363afe596a2d1e09d739
Matthew Brabham <matthew.brabham@oregonstate.edu>
2026-08-13 16:10:24 -0700

    Gaurd against sections not existing

--- modules/osu_library_hero/osu_library_hero.post_update.php
+++ modules/osu_library_hero/osu_library_hero.post_update.php
@@ -17,12 +17,15 @@ function osu_library_hero_post_update_preview_alt_text(): TranslatableMarkup {
     ->getStorage('section_library_template');
   /** @var array $section_library_items */
   $section_library_items = $section_storage->loadByProperties(['uuid' => '93f31c1f-c230-4449-a8cd-f5918aeb2853']);
-  /** @var \Drupal\section_library\Entity\SectionLibraryTemplate $section_library_item */
-  $section_library_item = reset($section_library_items);
-  /** @var \Drupal\image\Plugin\Field\FieldType\ImageItem $section_preview */
-  $section_preview = $section_library_item->get('image')[0];
-  $section_preview->set('alt', 'Example of a hero banner layout with centered text content and call-to-action button on a pink background featuring a repeating image pattern.');
-  $section_library_item->save();
+
+  if (count($section_library_items) > 0) {
+    /** @var \Drupal\section_library\Entity\SectionLibraryTemplate $section_library_item */
+    $section_library_item = reset($section_library_items);
+    /** @var \Drupal\image\Plugin\Field\FieldType\ImageItem $section_preview */
+    $section_preview = $section_library_item->get('image')[0];
+    $section_preview->set('alt', 'Example of a hero banner layout with centered text content and call-to-action button on a pink background featuring a repeating image pattern.');
+    $section_library_item->save();
+  }
 
   return t('Set alt text for Hero Library Preview');
 }
